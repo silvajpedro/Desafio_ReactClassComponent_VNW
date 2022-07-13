c99f29fc83f10041c9e808d1f4387b0ebb6b059d
João Pedro Belo <98714001+silvajpedro@users.noreply.github.com>
2022-07-12 21:22:18 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Estou usando o codesandbox</h1>
+        <h2>E estou importando o CSS</h2>
+      </div>
+    );
+  }
+}
